d1e41ebe94783f2bcc1d26566875aa0e6c57ce28
Denis <122015463+iziKomSD@users.noreply.github.com>
2024-05-17 10:00:47 +0300

    2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,35 +1,7 @@
 import "./App.css";
-import ContactForm from "../ContactForm/ContactForm";
-import SearchBox from "../SearchBox/SearchBox";
-import ContactList from "../ContactList/ContactList";
-import { useDispatch, useSelector } from "react-redux";
-import { selectError, selectLoading } from "../../redux/contactsSlice";
-import Loader from "../Loader/Loader";
-import Error from "../Error/Error";
-import { useEffect } from "react";
-import { fetchContacts } from "../../redux/contactsOps";
 
 function App() {
-  const dispatch = useDispatch();
-  const isLoading = useSelector(selectLoading);
-  const isError = useSelector(selectError);
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
-
-  return (
-    <>
-      <h1>Phonebook</h1>
-      <div className="contacts">
-        <ContactForm />
-        <SearchBox />
-        {isLoading && <Loader />}
-        {isError && <Error />}
-      </div>
-
-      <ContactList />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
---
+++ src/redux/auth/slice.js
@@ -0,0 +1,11 @@
+// Додайте у Redux новий стан auth наступної структури:
+
+// {
+//   user: {
+//     name: null,
+//     email: null,
+//   },
+//   token: null,
+//   isLoggedIn: false,
+//   isRefreshing: false,
+// }
