f6102abcd652295d35e88e9e151f70a7c85660e0
Denis <122015463+iziKomSD@users.noreply.github.com>
2024-05-17 18:59:41 +0300

    username

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,8 +1,15 @@
+import { useSelector } from "react-redux";
+import css from "../UserMenu/UserMenu.module.css";
+import { selectUser } from "../../redux/auth/selectors";
+
 export default function UserMenu() {
+  const user = useSelector(selectUser);
   return (
     <>
-      <p>Welcome username</p>
-      <button>Log</button>
+      <div className={css.styles}>
+        <p>Welcome, {user.name}</p>
+        <button>Log out</button>
+      </div>
     </>
   );
 }
