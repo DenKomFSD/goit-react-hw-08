f82901514693f9ae9e59553671073c7d7d0d413f
Denis <122015463+iziKomSD@users.noreply.github.com>
2024-05-18 20:43:41 +0300

    style 5 min-s

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,10 +1,20 @@
+import clsx from "clsx";
 import { NavLink } from "react-router-dom";
+import css from "./Navigation.module.css";
+
+const getNavLinkActive = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
 
 export default function Navigation() {
   return (
     <nav>
-      <NavLink to="/">Homepage</NavLink>
-      <NavLink to="/contacts">Contacts</NavLink>
+      <NavLink to="/" className={getNavLinkActive}>
+        Homepage
+      </NavLink>
+      <NavLink to="/contacts" className={getNavLinkActive}>
+        Contacts
+      </NavLink>
     </nav>
   );
 }
